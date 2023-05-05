a3f497f32967fa5c73074ad31750ebb04c27105b
Ronnie <ronniebercero@gmail.com>
2023-05-06 00:31:15 +0800

    Add createImgElem function

--- src/elemBuilder.js
+++ src/elemBuilder.js
@@ -2,6 +2,16 @@ function createElem(elem) {
     return document.createElement(elem);
 }
 
+function createImgElem(fileName, altName) {
+    const imgElem = createElem('img');
+
+    imgElem.src = `images/${fileName}`;
+    imgElem.alt = altName;
+
+    return imgElem;
+}
+
 export { 
     createElem,
+    createImgElem
 }
