66078d7008048667e28358553c8afa9d3ebbe034
Ronnie <ronniebercero@gmail.com>
2023-05-08 01:31:50 +0800

    Add createMenuBtn function

--- src/header.js
+++ src/header.js
@@ -1,5 +1,24 @@
 import { createElem, createImgElem, createAnchorElem } from "./elemBuilder";
 
+function createMenuBtn() {
+    const listString = ['HOME', 'MENU', 'CONTACT'];
+    const unorderedList = createElem('ul');
+
+    for (let i in listString) {
+        let anchor = createElem('a');
+        anchor.href = '';
+        anchor.classList.add('nav-btn');
+        anchor.classList.add(listString[i].toLowerCase());
+        anchor.innerText = listString[i];
+
+        let list = createElem('li');
+        list.appendChild(anchor);
+        unorderedList.appendChild(list);
+    }
+    
+    return unorderedList;
+}
+
 function loadHeader() {
     console.log('Header')
 }
