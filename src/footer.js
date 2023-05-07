f6bebaadfaf44e8fb4d80034c740b66ad51dc8e7
Ronnie <ronniebercero@gmail.com>
2023-05-08 01:06:59 +0800

    Add createCopyrightTxt function

--- src/footer.js
+++ src/footer.js
@@ -1,5 +1,11 @@
 import { createTextElem } from "./elemBuilder";
 
+function createCopyrightTxt() {
+    const copyrightTxt = createTextElem('© 2023 The Odin Project by onnieber', 'p', 'copyright-txt');
+
+    return copyrightTxt;
+}
+
 function loadFooter() {
     console.log('Footer');
 }
