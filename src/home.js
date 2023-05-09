c36f2b49679eae7f5366cfcf67d8ff93a0f1346d
Ronnie <ronniebercero@gmail.com>
2023-05-09 23:18:46 +0800

    Add createHomerContent function

--- src/home.js
+++ src/home.js
@@ -8,6 +8,16 @@ function createHeading() {
     return headingOne;
 }
 
+function createHomeContent() {
+    const homeContent = document.createElement('div');
+
+    homeContent.classList.add('hero-content');
+    homeContent.appendChild(createHeading());
+    homeContent.appendChild(createBtnElem('VIEW MENU', 'view-menu'));
+
+    return homeContent;
+}
+
 function loadHome() {
     console.log('Home');
 }
