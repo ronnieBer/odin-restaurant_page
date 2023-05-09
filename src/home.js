9df9cb830c23bc6da52cb0872005a55654bb0183
Ronnie <ronniebercero@gmail.com>
2023-05-09 23:17:03 +0800

    Add createHeading function

--- src/home.js
+++ src/home.js
@@ -1,5 +1,13 @@
 import { createElem, createTextElem, createSpanElem, createBtnElem } from "./elemBuilder";
 
+function createHeading() {
+    const headingOne = createTextElem('Sharing a passion for ', 'h1', 'hero-heading');
+    
+    headingOne.appendChild(createSpanElem('sweets, creations, and experiences.'));
+
+    return headingOne;
+}
+
 function loadHome() {
     console.log('Home');
 }
