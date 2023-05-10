101658ac90f994fc481a61b71ef227e3e38833b4
Ronnie <ronniebercero@gmail.com>
2023-05-11 00:38:12 +0800

    Add createHeader function

--- src/loadPage.js
+++ src/loadPage.js
@@ -3,6 +3,12 @@ import loadHeader from "./header";
 import loadHome from "./home";
 import loadFooter from "./footer";
 
+function createHeader() {
+    const header = createElem('header');
+
+    return header;
+}
+
 function initializedPage() {
     console.log('Hello World');
 }
