8bde3b598a930abf6e3b022321768f6bba7c94b2
Ronnie <ronniebercero@gmail.com>
2023-05-19 23:14:44 +0800

    Add createMenuItem function

--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,25 @@
 import { createElem, createImgElem, createTextElem, createBtnElem } from "./elemBuilder";
 
+function createMenuItem(fileUrl, itemName, itemPrice) {
+    const menuItem = createElem('div');
+    const menuItemName = createElem('div');
+    const menuItemPrice = createElem('div');
+
+    menuItemName.classList.add('menu-item-name');
+    menuItemName.appendChild(createTextElem(itemName, 'h4', 'item-name'));
+
+    menuItemPrice.classList.add('menu-item-price');
+    menuItemPrice.appendChild(createTextElem(itemPrice, 'p', 'item-price'));
+
+    menuItem.classList.add('menu-item');
+    menuItem.appendChild(createImgElem(fileUrl, itemName.toLowerCase()));
+    menuItem.appendChild(menuItemName);
+    menuItem.appendChild(menuItemPrice);
+    menuItem.appendChild(createBtnElem('View Details', 'view-details-btn'));
+
+    return menuItem;
+}
+
 function loadMenu() {
     console.log('Menu');
 }
