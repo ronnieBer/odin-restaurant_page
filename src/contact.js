aeb824ce6d5c562561ddbdaadb12300538267867
Ronnie <ronniebercero@gmail.com>
2023-05-22 00:43:54 +0800

    Import createTextareaElem function

--- src/contact.js
+++ src/contact.js
@@ -1,4 +1,4 @@
-import { createElem, createImgElem, createTextElem, createInputElem } from "./elemBuilder";
+import { createElem, createImgElem, createTextElem, createInputElem, createTextareaElem } from "./elemBuilder";
 
 function loadContact() {
     console.log('Contact');
